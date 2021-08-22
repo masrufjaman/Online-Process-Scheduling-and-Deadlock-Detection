ae7360a88b0611b351d3d363069f19916e7bb0da
Md. Masruf Jaman <masruf.jaman@northsouth.edu>
2021-08-22 15:17:28 +0600

    process scheduling link up

--- home/views.py
+++ home/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home/home.html')
+
+
+def ps(request):
+    return render(request, 'home/ps.html', {'title': 'Process Scheduling'})
